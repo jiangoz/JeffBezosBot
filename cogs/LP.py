8751f89e786ad1bdb39cd611c40d3c15055f840b
jiango <yjiang2001@gmail.com>
2022-11-12 00:58:54 -0500

    bold lp title and new line

--- cogs/LP.py
+++ cogs/LP.py
@@ -11,7 +11,7 @@ class LP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.lp = {
-            'Customer Obsession': 'Leaders start with the customer and work backwards. They work vigorously to earn and keep customer trust. Although leaders pay attention to competitors, they obsess over customers.',
+            'Customer Obsession': '**Customer Obsession**\nLeaders start with the customer and work backwards. They work vigorously to earn and keep customer trust. Although leaders pay attention to competitors, they obsess over customers.',
             'Ownership': 'Leaders are owners. They think long term and don’t sacrifice long-term value for short-term results. They act on behalf of the entire company, beyond just their own team. They never say “that’s not my job."',
             'Invent and Simplify': 'Leaders expect and require innovation and invention from their teams and always find ways to simplify. They are externally aware, look for new ideas from everywhere, and are not limited by “not invented here." As we do new things, we accept that we may be misunderstood for long periods of time.',
             'Are Right, A Lot': 'Leaders are right a lot. They have strong judgment and good instincts. They seek diverse perspectives and work to disconfirm their beliefs.',
