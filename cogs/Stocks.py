538b6805c2b09533d4b55fac84c378b6667977e8
jiango <17531569+jiangoz@users.noreply.github.com>
2024-01-16 23:02:13 -0500

    debug ticker obj

--- cogs/Stocks.py
+++ cogs/Stocks.py
@@ -44,8 +44,10 @@ class Stocks(commands.Cog):
         """Look up a stock and graph it"""
         
         stock = yf.Ticker(ticker_symbol.upper())
+        print(f'DEBUG: {stock}')
         try:
             stock_info = stock.info
+            print(f'DEBUG: {stock_info}')
         except:
             await interaction.response.send_message('Stock info not found. Please try again.', 
                                                     ephemeral=True)
